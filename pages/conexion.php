f48a7bf755343ee956e972af386be1c2cd7dd32f
AJSanchez8 <antoniojosesanchezgonzalez8@gmail.com>
2024-01-29 14:29:52 +0100

    Final

--- pages/conexion.php
+++ pages/conexion.php
@@ -1,10 +1,10 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'localhost');
 define('DB_USER', 'username_here');
 define('DB_PASSWORD', 'password_here');
 
-$conexion = new mysqli(DB_HOST,DB_NAME,DB_USER,DB_PASSWORD);
+$conexion = new mysqli('localhost','localhost','username_here','password_here');
 
 ?>
