d7eb3a4f5c594dea1490f1b24ba5fb81a5d1e02c
AJSanchez8 <antoniojosesanchezgonzalez8@gmail.com>
2024-01-29 14:37:28 +0100

    Final

--- bd/tabla.sql
+++ bd/tabla.sql
@@ -1,9 +1,7 @@
--- Create a database
 DROP DATABASE IF EXISTS lamp_db;
 CREATE DATABASE lamp_db CHARSET utf8mb4;
 USE lamp_db;
 
--- Create the tareas table
 CREATE TABLE tareas (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   titulo VARCHAR(100) NOT NULL,
