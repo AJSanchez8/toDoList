fde3f357dd882078b203f0bbc32fd4781c1f3a18
AJSanchez8 <antoniojosesanchezgonzalez8@gmail.com>
2024-01-29 14:05:50 +0100

    Configurando todo

--- bd/tabla.sql
+++ bd/tabla.sql
@@ -1,61 +1,22 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: mysql
--- Tiempo de generación: 18-01-2024 a las 08:27:07
--- Versión del servidor: 8.1.0
--- Versión de PHP: 8.2.8
+-- Create a database
+DROP DATABASE IF EXISTS lamp_db;
+CREATE DATABASE lamp_db CHARSET utf8mb4;
+USE lamp_db;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Create the tareas table
+CREATE TABLE tareas (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(100) NOT NULL,
+  descripcion VARCHAR(300) NOT NULL,
+  fecha DATE NOT NULL,
+  realizado TINYINT(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+/*
+-- Example about how to create a new database user and grant privileges.
+-- Remember to change the static values of: DB_USER, DB_PASSWORD and DB_NAME
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `agenda`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tareas`
---
-
-CREATE TABLE `tareas` (
-  `id` int UNSIGNED NOT NULL,
-  `titulo` varchar(100) NOT NULL,
-  `descripcion` varchar(300) NOT NULL,
-  `fecha` date NOT NULL,
-  `realizado` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `tareas`
---
-ALTER TABLE `tareas`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `tareas`
---
-ALTER TABLE `tareas`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP USER IF EXISTS 'DB_USER'@'%';
+CREATE USER 'DB_USER'@'%' IDENTIFIED BY 'DB_PASSWORD';
+GRANT ALL PRIVILEGES ON lamp_db.* TO 'DB_USER'@'%';
+*/
